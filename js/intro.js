ad557ed1ca2b889a348419b0d77790426283b152
Abhilash R <46276387+Neo000001@users.noreply.github.com>
2026-02-03 20:29:40 +0530

    Change redirect timeout after book opening animation
    Updated the timeout for redirecting to the home page after the book opening animation.

--- js/intro.js
+++ js/intro.js
@@ -1,6 +1,13 @@
 function openBook() {
   document.querySelector(".book").classList.add("open");
+
+  // show loader after book starts opening
+  setTimeout(() => {
+    document.getElementById("loader").classList.add("show");
+  }, 600);
+
+  // go to home page after animation
   setTimeout(() => {
     window.location.href = "home.html";
-  }, 1200);
+  }, 1600);
 }
